83c3f06a2ca4296ea2f0490acb53674174b1c535
eye-scream <mail@eye-scream.net>
2021-09-05 11:16:06 +0200

    add runtime and memory usage

--- go/01_two_sum.go
+++ go/01_two_sum.go
@@ -1,3 +1,6 @@
+// Runtime: 8 ms
+// Memory Usage: 4,3 MB
+
 func twoSum(nums []int, target int) []int {
 	m := make(map[int]int)
 	for index, num := range nums {
--- go/09_palindrome_number.go
+++ go/09_palindrome_number.go
@@ -1,26 +1,28 @@
+// Runtime: 20 ms
+// Memory Usage: 5.1 MB
+
 func isPalindrome(number int) bool {
-    
-    if number < 0 {
-        return false
-    }
-    
-    var remainder,temp int
-    var reverse int = 0
-    temp=number
-    
+	if number < 0 {
+		return false
+	}
+
+	var remainder, temp int
+	var reverse int = 0
+	temp = number
+
 	for {
-		remainder = number%10
+		remainder = number % 10
 		reverse = reverse*10 + remainder
 		number /= 10
 
-		if(number==0){
-			break 
+		if number == 0 {
+			break
 		}
 	}
 
-	if(temp==reverse){
+	if temp == reverse {
 		return true
-	}else{
+	} else {
 		return false
 	}
 }
